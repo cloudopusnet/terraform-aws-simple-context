ec4c792ee5b1d60968177acae6c0f21c7d8b43ac
Sandor Bakos <sandor@cloudopus.net>
2024-10-25 20:49:05 +0200

    Context description is added

--- variables.tf
+++ variables.tf
@@ -1,4 +1,11 @@
 variable "context" {
+  description = <<-EOF
+    Context Object
+    - unique_short_id for uniquely identifiable prefix
+    - environment for a dedicated environment like (dev, staging, uat, prov, pr-x)
+    - additional_context list of strings to specify to include in the context
+    - additional_tags map of tags to be added
+  EOF
   type = object({
     unique_short_id    = optional(string)
     environment        = optional(string)
